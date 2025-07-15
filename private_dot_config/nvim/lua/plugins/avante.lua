5a5f004689dac298653f3536d371942d47fc4480
Alexey Lukomskiy <lucomsky@gmail.com>
2025-07-15 14:14:14 +0800

    ZSH and LazyVim conf updates

--- private_dot_config/nvim/lua/plugins/avante.lua
+++ private_dot_config/nvim/lua/plugins/avante.lua
@@ -4,7 +4,20 @@ return {
   lazy = false,
   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
   opts = {
-    -- add any opts here
+    providers = {
+      claude = {
+        endpoint = "https://api.anthropic.com",
+        -- model = "claude-3-7-sonnet",
+        -- model = "claude-3-7-sonnet-20250219",
+        model = "claude-sonnet-4-20250514",
+        timeout = 30000, -- Timeout in milliseconds
+        extra_request_body = {
+          temperature = 0,
+        },
+        -- max_tokens = 4096,
+        -- disable_tools = true, -- disable tools!
+      },
+    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
