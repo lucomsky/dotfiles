fb665f9da89fcd98330878c9645e1bd8a91b62f6
Alexey Lukomskiy <lucomsky@gmail.com>
2026-03-26 11:19:52 +0700

    aliases for jwtd, espdif

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -1,1 +1,10 @@
 alias cd="z"
+
+jwtd() {
+    if [[ -x $(command -v jq) ]]; then
+         jq -R 'split(".") | .[0],.[1] | @base64d | fromjson' <<< "${1}"
+         echo "Signature: $(echo "${1}" | awk -F'.' '{print $3}')"
+    fi
+}
+
+alias get_idf='. $HOME/repos/esp-idf/export.sh'
